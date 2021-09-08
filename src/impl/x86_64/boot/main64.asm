4f5e739b4dafb22feb2442faf406fce1d0dbb93d
billyeatcookies <billydevbusiness@gmail.com>
2021-09-08 06:57:05 -0500

    basic command line utilities - print characters - print strings - colors Sample: - prints "Hello, world!" in yellow color using c programming language now

--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -1,4 +1,5 @@
 global long_mode_start
+extern kernel_main
 
 section .text
 bits 64
@@ -11,7 +12,5 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
 
-    ; print `OK`
-    mov dword [0xb8000], 0x2f4b2f4f
-
+    call kernel_main
     hlt
