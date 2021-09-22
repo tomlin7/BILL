17031e324cc1a49a70957dc33b57093f9dd2277a
VeryCoolMoment <64923791+VeryCoolMoment@users.noreply.github.com>
2021-09-22 16:36:01 +0100

    Update panic.c

--- src/impl/kernel/panic.c
+++ src/impl/kernel/panic.c
@@ -1,7 +1,7 @@
 #include <panic.h>
 #include <print.h>
 
-// Shitty Implementation of a PANIC System, This is not good tbh
+
 void panic(){
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
     print_clear();
