157e230eea47f5b92848ab8fe1453b132dde1538
Billy <billydevbusiness@gmail.com>
2023-08-07 18:43:49 +0800

    fix: path errors

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
-cd kernel
 make
-cd ..
-
 git clone https://github.com/limine-bootloader/limine.git --branch=v5.x-branch-binary --depth=1
 make -C limine
 mkdir -p iso_root
-cp -v kernel/bill.elf limine.cfg limine/limine-bios.sys \
+cp -v bill.elf limine.cfg limine/limine-bios.sys \
       limine/limine-bios-cd.bin limine/limine-uefi-cd.bin iso_root/
 mkdir -p iso_root/EFI/BOOT
 cp -v limine/BOOTX64.EFI iso_root/EFI/BOOT/
