8d15a70c1aca7396170ec23b58a82e79d6de6e3f
Monoswini Dey <110888282+Mandy-shr@users.noreply.github.com>
2024-02-06 15:05:08 +0530

    updated

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,6 +4,6 @@
 void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Hello, world!");
+    print_str("Very slow good for nothing");
     panic();
 }
